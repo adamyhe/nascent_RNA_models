ee642994c856048623145e983c5ac4e6067a0441
Adam Youlin He <ayh8@cornell.edu>
2024-03-30 21:54:32 -0400

    Update train.py

--- src/2_train_models/train.py
+++ src/2_train_models/train.py
@@ -4,6 +4,7 @@ import sys
 # Script inputs: expecting cell type, model_type, fold #, maybe gpu
 
 assert len(sys.argv) in [5, 6], len(sys.argv)
+print(len(sys.argv))
 
 cell_type, model_type, data_type, fold = sys.argv[1:5]
 
